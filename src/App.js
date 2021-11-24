313f03ef18304a4145bb8e8967b1b395376996c9
Hakan Ünlü <unluhak@outlook.com>
2021-11-25 00:11:02 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Player from './components/Player'
 import Song from './components/Song'
+import { useState } from 'react'
 
 //Styles
 import './styles/app.scss'
 
+//Util
+import data from './util'
+
 function App() {
+  //STATE
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[3])
   return (
     <div className='App'>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   )
