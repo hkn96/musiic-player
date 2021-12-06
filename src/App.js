1f3c1dc30e1a245e596dcb499b42c80f36e741c0
Hakan Ünlü <unluhak@outlook.com>
2021-12-06 22:17:16 +0100

    Mobile gemacht

--- src/App.js
+++ src/App.js
@@ -20,13 +20,22 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   })
   //toggle library button
   const [libraryStatus, setLibraryStatus] = useState(false)
   const timeUpdateHandler = e => {
     const current = e.target.currentTime
     const duration = e.target.duration
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration })
+    const roundedCurrent = Math.round(current)
+    const roundedDuration = Math.round(duration)
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100)
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: animation,
+    })
   }
   return (
     <div className='App'>
@@ -39,6 +48,9 @@ function App() {
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
       />
       <Library
         isPlaying={isPlaying}
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faPlay,
@@ -14,7 +14,28 @@ const Player = ({
   setIsPlaying,
   setSongInfo,
   songInfo,
+  songs,
+  setCurrentSong,
+  setSongs,
 }) => {
+  //useEffect
+
+  useEffect(() => {
+    const newSongs = songs.map(song => {
+      if (song.id === currentSong.id) {
+        return {
+          ...song,
+          active: true,
+        }
+      } else {
+        return {
+          ...song,
+          active: false,
+        }
+      }
+    })
+    setSongs(newSongs)
+  }, [currentSong])
   //State
 
   //event handler
@@ -38,21 +59,50 @@ const Player = ({
     setSongInfo({ ...songInfo, currentTime: e.target.value })
   }
 
+  const skipTrackHandler = direction => {
+    let currentIndex = songs.findIndex(song => song.id === currentSong.id)
+    if (direction === 'skip-forward') {
+      setCurrentSong(songs[(currentIndex + 1) % songs.length])
+    }
+    if (direction === 'skip-back') {
+      if ((currentIndex - 1) % songs.length === -1) {
+        setCurrentSong(songs[songs.length - 1])
+        return
+      }
+      setCurrentSong(songs[(currentIndex - 1) % songs.length])
+    }
+  }
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  }
+
   return (
     <div className='player'>
       <div className='time-control'>
         <p>{getTime(songInfo.currentTime)}</p>
-        <input
-          onChange={dragHandler}
-          min={0}
-          max={songInfo.duration || 0}
-          value={songInfo.currentTime}
-          type='range'
-        />
-        <p>{getTime(songInfo.duration)}</p>
+        <div
+          style={{
+            background: `linear-gradient(to right,${currentSong.color[0]},${currentSong.color[1]})`,
+          }}
+          className='track'>
+          <input
+            onChange={dragHandler}
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            type='range'
+          />
+          <div style={trackAnim} className='animate-track'></div>
+        </div>
+        <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
       </div>
       <div className='play-control'>
-        <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler('skip-back')}
+          className='skip-back'
+          size='2x'
+          icon={faAngleLeft}
+        />
         <FontAwesomeIcon
           onClick={playHandler}
           className='play'
@@ -61,6 +111,7 @@ const Player = ({
         />
 
         <FontAwesomeIcon
+          onClick={() => skipTrackHandler('skip-forward')}
           className='skip-forward'
           size='2x'
           icon={faAngleRight}
